89b567ba175c5c41807dd77ce1b72e2f60053fed
Meron Tesfai <meron.tesfai@hftm.ch>
2025-06-12 20:45:52 +0200

    feat: demoApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DemoAppComponent } from './demo-app/demo-app.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DemoAppComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Blog-Frontend-Meron-Tesfai';
